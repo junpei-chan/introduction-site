dbc8241ed19201c4dcb02bd0bf0b2ed8daa954d5
junpei-chan <jumpei24s1102270@gmail.com>
2026-02-22 13:31:26 +0900

    [add] ButtonWithFavoriteコンポーネントの作成

---
+++ src/components/favorite/button-with-favorite.tsx
@@ -0,0 +1,31 @@
+interface Props {
+  key: number;
+  text: string;
+  color: "turquoise-blue" | "mustard-yellow";
+  onClick?: () => void;
+}
+
+export function ButtonWithFavorite({ 
+  key, 
+  text, 
+  color,
+  onClick,
+}: Props) {
+  const isBlue = color === "turquoise-blue";
+
+  return (
+    <button
+      key={key}
+      onClick={onClick}
+      className={`
+        flex items-center justify-center w-40 h-11 bg-off-white text-gray-800 body-16-medium rounded-lg cursor-pointer m-1.5
+        ${isBlue
+          ? "shadow-[0_0_0_6px_var(--color-turquoise-blue-500),0_6px_0_6px_rgba(49,112,104,0.5)]"
+          : "shadow-[0_0_0_6px_var(--color-mustard-yellow-400),0_6px_0_6px_rgba(167,165,26,0.5)]"
+        }
+      `}
+    >
+      {text}
+    </button>
+  );
+}
