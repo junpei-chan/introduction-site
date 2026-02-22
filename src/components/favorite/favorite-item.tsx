3d300fbf3accde64819a1874c39f56e688a5ed42
junpei-chan <jumpei24s1102270@gmail.com>
2026-02-22 23:50:58 +0900

    [add] 好きなものにgameのジャンルを追加

--- src/components/favorite/favorite-item.tsx
+++ src/components/favorite/favorite-item.tsx
@@ -20,7 +20,7 @@ export function FavoriteItem({
       />
       <div
         className={`
-          flex flex-col gap-4 w-full h-full bg-off-white px-6 py-4 shadow-[4px_4px_4px_2px_rgba(98,162,153,0.25)] hover:rotate-3 transition-all whitespace-nowrap
+          flex flex-col gap-4 w-full h-full bg-off-white px-6 py-4 shadow-[4px_4px_4px_2px_rgba(98,162,153,0.25)] hover:rotate-3 transition-all overflow-hidden
           ${isWobbling ? "animate-wobble" : ""}
         `}
         onMouseLeave={() => setIsWobbling(true)}
@@ -28,9 +28,9 @@ export function FavoriteItem({
       >
         <div
           style={{ backgroundImage: `url('${imagePath}')` }}
-          className="w-full h-36 rounded-lg bg-no-repeat bg-center bg-cover border-gray-500 border-2"
+          className="w-full flex-1 min-h-0 rounded-lg bg-no-repeat bg-center bg-cover border-gray-500 border-2"
         />
-        <p>{name}</p>
+        <p className="line-clamp-2">{name}</p>
         <svg width="100%" height="4">
           <line
             x1="0" y1="2" x2="100%" y2="2"
