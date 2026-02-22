89f6fffe06b7fb6b4ae44abce555c767e7b075b6
junpei-chan <jumpei24s1102270@gmail.com>
2026-02-22 23:02:59 +0900

    [add] 好きなものにcolor・designジャンルを追加

--- src/components/favorite/favorite-list.tsx
+++ src/components/favorite/favorite-list.tsx
@@ -1,7 +1,31 @@
 import { FavoriteItem } from "./favorite-item";
-import MockData from "../../../data/favorites/food.json";
+import FoodData from "../../../data/favorites/food.json";
+import ColorDesignData from "../../../data/favorites/color-design.json";
+import GameData from "../../../data/favorites/game.json";
+import CharacterData from "../../../data/favorites/character.json";
+import EntertainmentData from "../../../data/favorites/entertainment.json";
+import TypeData from "../../../data/favorites/type.json";
+import OtherData from "../../../data/favorites/other.json";
+
+interface Item { 
+  id: number; 
+  imagePath: string; 
+  name: string 
+};
+
+const dataMap: Record<string, Item[]> = {
+  "食べもの": FoodData,
+  "カラー・デザイン": ColorDesignData,
+  "ゲーム": GameData,
+  "キャラクター": CharacterData,
+  "エンタメ": EntertainmentData,
+  "タイプ": TypeData,
+  "その他": OtherData,
+};
 
 export function FavoriteList({title}: {title: string}) {
+  const items = dataMap[title] ?? [];
+
   return (
     <section className="w-260 mx-auto bg-white p-6 rounded-2xl">
       <div className="flex flex-col justify-center gap-12 bg-turquoise-blue-200 py-12 rounded-xl">
@@ -18,7 +42,7 @@ export function FavoriteList({title}: {title: string}) {
           />
         </div>
         <div className="inline-grid grid-cols-3 gap-8 mx-auto">
-          {MockData.map((item) => (
+          {items.map((item) => (
             <FavoriteItem
               key={item.id}
               imagePath={item.imagePath}
